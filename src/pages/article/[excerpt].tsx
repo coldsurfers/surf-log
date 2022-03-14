a170552e529aeab7594411b9715a7229b454b4b9
yungblud <killi8n@gmail.com>
2022-03-14 23:32:20 +0900

    feata: added excerpt page

--- src/pages/article/[excerpt].tsx
+++ src/pages/article/[excerpt].tsx
@@ -1,7 +1,63 @@
-import { NextPage } from 'next'
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
+import { Article } from '../../types/article'
 
-const Excerpt: NextPage = () => {
-    return null
+const Excerpt: NextPage<{ article?: Article | null }> = (props) => {
+    const { article } = props
+    if (!article) {
+        return null
+    }
+    return (
+        <div>
+            <p>{article.data.title}</p>
+            <p>{article.content}</p>
+        </div>
+    )
+}
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+    const res = await fetch('http://localhost:3000/article-meta.json')
+    const articleMeta = (await res.json()) as {
+        articles: {
+            [key: string]: Article
+        }
+    }
+    return {
+        paths: Object.entries(articleMeta.articles).map(([key, content]) => ({
+            params: {
+                excerpt: content.excerpt,
+            },
+        })),
+        fallback: true, // false or 'blocking'
+    }
+}
+
+export const getStaticProps: GetStaticProps<
+    {
+        article: Article | null
+    },
+    {
+        excerpt: string
+    }
+> = async (ctx) => {
+    if (!ctx.params) {
+        return {
+            props: {
+                article: null,
+            },
+        }
+    }
+    const { excerpt } = ctx.params
+    const res = await fetch('http://localhost:3000/article-meta.json')
+    const articleMeta = (await res.json()) as {
+        articles: {
+            [key: string]: Article
+        }
+    }
+    return {
+        props: {
+            article: articleMeta.articles[encodeURI(excerpt)],
+        },
+    }
 }
 
 export default Excerpt
