667c33e0d5f0ae6a1c0fd9aa4b93fb66cd77b80c
yungblud <killi8n@gmail.com>
2022-03-15 15:53:36 +0900

    feat: added emotion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,20 @@
 import type { AppProps } from 'next/app'
+import { Global, css } from '@emotion/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Component {...pageProps} />
+            <Global
+                styles={css`
+                    body {
+                        font-family: ui-monospace, SFMono-Regular, SF Mono,
+                            Menlo, Consolas, Liberation Mono, monospace !important;
+                    }
+                `}
+            />
+        </>
+    )
 }
 
 export default MyApp
