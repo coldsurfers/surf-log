0e6d1f24cb6cddd7019f67c1fb64a7a79d60d367
yungblud <killi8n@gmail.com>
2022-03-16 18:24:04 +0900

    feat: added breakpoints

---
+++ src/types/breakpoints.ts
@@ -0,0 +1,13 @@
+export enum BREAK_POINTS_SIZE {
+    SMALL = 'small',
+    MEDIUM = 'medium',
+    LARGE = 'large',
+    XLARGE = 'xlarge',
+}
+
+export enum BREAK_POINTS_METRIC {
+    SMALL = 350,
+    MEDIUM = 690,
+    LARGE = 1200,
+    XLARGE = 1800,
+}
