a932ae12facdb0ca35e03a21cab31d2817afc780
yungblud <killi8n@gmail.com>
2022-03-19 16:40:27 +0900

    refactor: refactored Article interface

--- src/types/article.ts
+++ src/types/article.ts
@@ -1,13 +1,15 @@
 export interface Article {
     content: string
-    data: {
-        title?: string
-        category?: string
-        createdAt?: string
-        excerpt?: string
-        thumbnail?: string | null
-    }
+    data: ArticleData
     excerpt: string
     isEmpty: boolean
     thumbnailBase64?: string
+}
+
+export interface ArticleData {
+    title?: string
+    category?: string
+    createdAt?: string
+    excerpt?: string
+    thumbnail?: string | null
 }
