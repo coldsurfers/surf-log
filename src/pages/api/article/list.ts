0a250dac1a98f443e564d4ff83ab6dac12e4519d
yungblud <killi8n@gmail.com>
2022-03-29 13:56:18 +0900

    feat: api method handling for article list api

--- src/pages/api/article/list.ts
+++ src/pages/api/article/list.ts
@@ -1,5 +1,17 @@
 import { NextApiHandler } from 'next'
+import meta from '../../../../public/article-meta.json'
 
-const ArticleListAPI: NextApiHandler = (req, res) => {}
+const ArticleListAPI: NextApiHandler = (req, res) => {
+    const { articles, categories } = meta
+
+    switch (req.method) {
+        case 'GET':
+            return res.status(200).json({})
+        default:
+            return res.status(405).json({
+                error: 'Method not allowed',
+            })
+    }
+}
 
 export default ArticleListAPI
