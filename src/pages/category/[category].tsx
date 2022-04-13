24d83a63d69bfe669bd6c50c919272bc7487ea39
yungblud <killi8n@gmail.com>
2022-04-13 22:54:37 +0900

    feat: applied react-query on category page

--- src/pages/category/[category].tsx
+++ src/pages/category/[category].tsx
@@ -1,23 +1,64 @@
 import { GetServerSideProps, NextPage } from 'next'
 import Head from 'next/head'
-import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { useCallback, useEffect, useState } from 'react'
+import { useQuery } from 'react-query'
 import ArticleListTemplate from '../../components/templates/ArticleListTemplate'
 import fetcher from '../../lib/fetcher'
 import { Article } from '../../types/article'
 
 interface ServerProps {
-    articles: Article[]
-    error?: string
-    category: string
+    initialArticles: Article[]
 }
 
-const Category: NextPage<ServerProps> = (props) => {
-    const { articles, error, category } = props
+const Category: NextPage<ServerProps> = ({ initialArticles }) => {
+    const router = useRouter()
+    const { category } = router.query
+    const [articles, setArticles] = useState<Article[]>(initialArticles)
+    const [page, setPage] = useState<number>(1)
+    const [mounted, setMounted] = useState<boolean>(false)
+
     useEffect(() => {
-        if (error) {
-            console.error(error)
+        setMounted(true)
+    }, [])
+
+    const { data, isFetching } = useQuery(
+        ['getArticleList', page, category],
+        async (params) => {
+            const [key, page, category] = params.queryKey as [
+                string,
+                number,
+                string
+            ]
+            const res = await fetcher.articleList({
+                page,
+                category,
+            })
+            return res.list
+        },
+        {
+            initialData: !mounted ? initialArticles : [],
         }
-    }, [error])
+    )
+
+    const onLoadMore = useCallback(() => {
+        if (isFetching) return
+        setPage((prev) => prev + 1)
+    }, [isFetching])
+
+    useEffect(() => {
+        if (!data) return
+        setArticles((prev) => {
+            if (page === 1 && !mounted) return prev
+            return prev.concat(data)
+        })
+    }, [data, mounted, page])
+
+    useEffect(() => {
+        setPage(1)
+        setArticles([])
+    }, [category])
+
     return (
         <>
             <Head>
@@ -28,7 +69,11 @@ const Category: NextPage<ServerProps> = (props) => {
                     content={`${category} category of ColdSurf blog`}
                 />
             </Head>
-            <ArticleListTemplate articles={articles} />
+            <ArticleListTemplate
+                articles={articles}
+                onLoadMore={onLoadMore}
+                isLoading={isFetching}
+            />
         </>
     )
 }
@@ -39,11 +84,10 @@ export const getServerSideProps: GetServerSideProps<
         category: string
     }
 > = async (ctx) => {
-    if (!ctx.params) {
+    if (!ctx.params?.category) {
         return {
             props: {
-                articles: [],
-                category: '',
+                initialArticles: [],
             },
         }
     }
@@ -52,8 +96,7 @@ export const getServerSideProps: GetServerSideProps<
 
     return {
         props: {
-            articles: list,
-            category,
+            initialArticles: list,
         },
     }
 }
