e7a9fd0e9149ff5c649b23ce7135ea13172ebd87
yungblud <killi8n@gmail.com>
2022-04-13 22:11:05 +0900

    feat: applied react-query on index page with ArticleListTemplate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,43 @@ import ArticleListTemplate from '../components/templates/ArticleListTemplate'
 import fetcher from '../lib/fetcher'
 import { Article } from '../types/article'
 import Head from 'next/head'
+import { useQuery } from 'react-query'
+import { useCallback, useEffect, useState } from 'react'
 
 interface ServerProps {
-    articles: Article[]
+    initialArticles: Article[]
 }
 
 const Home: NextPage<ServerProps> = (props) => {
-    const { articles } = props
+    const { initialArticles } = props
+    const [articles, setArticles] = useState<Article[]>(initialArticles)
+    const [page, setPage] = useState<number>(1)
+    const { data, isFetching } = useQuery(
+        ['getArticleList', page],
+        async (params) => {
+            const [key, page] = params.queryKey as [string, number]
+            const res = await fetcher.articleList({
+                page,
+            })
+            return res.list
+        },
+        {
+            initialData: articles.length === 0 ? initialArticles : [],
+        }
+    )
+
+    const onLoadMore = useCallback(() => {
+        if (isFetching) return
+        setPage((prev) => prev + 1)
+    }, [isFetching])
+
+    useEffect(() => {
+        if (!data) return
+        setArticles((prev) => {
+            if (page === 1 && prev.length > 0) return prev
+            return prev.concat(data)
+        })
+    }, [data, page])
 
     return (
         <>
@@ -20,7 +50,11 @@ const Home: NextPage<ServerProps> = (props) => {
                     content="Welcome to ColdSurf blog"
                 />
             </Head>
-            <ArticleListTemplate articles={articles} />
+            <ArticleListTemplate
+                articles={articles}
+                onLoadMore={onLoadMore}
+                isLoading={isFetching}
+            />
         </>
     )
 }
@@ -32,7 +66,7 @@ export const getServerSideProps: GetServerSideProps<ServerProps> = async (
 
     return {
         props: {
-            articles: list,
+            initialArticles: list,
         },
     }
 }
