81e0e847fb095d81b822caf43562c41b75fa731c
yungblud <killi8n@gmail.com>
2023-03-11 21:18:06 +0900

    feat: :zap: removed unnecessary local fetch

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,7 +1,5 @@
-import { Article } from '../types/article'
 import { ArticleMeta } from '../types/articleMeta'
 import { EditorSaveModalValues } from '../types/modal'
-import { DEFAULT_PAGINATION_COUNT } from './constants'
 
 const headers = new Headers({
     'Content-Type': 'application/json',
@@ -13,10 +11,6 @@ const { PAGE_API_PRE_URL: preURL } = process.env
 interface GetTempSavedData {
     error: string | null
     tempArticleText: string
-}
-
-interface GetArticleByExcerptData {
-    data: Article | null
 }
 
 interface SaveArticleData {
@@ -96,38 +90,6 @@ const fetcher = {
         })
         const data = (await res.json()) as TemporarySaveArticleData
         return data
-    },
-    articleList: async function ({
-        page,
-        category,
-        tag,
-    }: {
-        page: number
-        category?: string
-        tag?: string
-    }): Promise<{
-        list: Article[]
-    }> {
-        const { articles } =
-            require('../../public/article-meta.json') as ArticleMeta
-        let list = Object.entries(articles).map(([key, data]) => data)
-        if (category) {
-            list = list.filter((data) => {
-                return data.data.category === category
-            })
-        }
-        if (tag) {
-            list = list.filter((data) => {
-                return data.data.tags?.includes(tag)
-            })
-        }
-        list = list.slice(
-            (page - 1) * DEFAULT_PAGINATION_COUNT,
-            page * DEFAULT_PAGINATION_COUNT
-        )
-        return {
-            list,
-        }
     },
     getArticleMeta: function (): { articleMeta: ArticleMeta } {
         const articleMeta =
